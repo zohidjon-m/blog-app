84e86ca1cb210790ff64886ef1693b152ad82ae9
Mahmudjonov Zohidjon <zohidbekmahmudjanov03@gmail.com>
2024-07-30 12:01:53 +0900

    Add cookies

--- fikrlog/app.py
+++ fikrlog/app.py
@@ -1,10 +1,31 @@
-import os
-from slugify import slugify
-from flask import (Flask, render_template, request)
+
+from flask import (Flask, render_template, request, session, make_response)
 from article import Article
 app = Flask(__name__)
 
 articles = Article.all()
+
+
+@app.route("/set-session")
+def set_session():
+    session["user_id"] = 1
+    return "session set"
+
+@app.route("/get-session")
+def get_session():
+    return f"user_id = {session['user_id']}"
+
+@app.route("/first-time")
+def first_time():
+    if 'seen' not in request.cookies:
+       response = make_response("you are new here")
+       response.set_cookie("seen", "1")
+       return response
+   
+    seen = int(request.cookies["seen"])
+    response = make_response(f"I have seen you before {seen} times")
+    response.set_cookie("seen", str(seen+1))
+    return response
 
 @app.route("/")
 def blog():
@@ -15,8 +36,7 @@ def article(slug: str):
     article = articles[slug]
     return render_template("article.html", article = article)
 
-@app.route("/blog/new_article")
-def new_article():
-    return render_template("new_article.html")
+
 if __name__ == "__main__":
-    app.run(port=4440, debug=True)
+    app.run(port=4444, debug=True)
+
--- fikrlog/article.py
+++ fikrlog/article.py
@@ -1,26 +1,26 @@
 import os
-from slugify import slugify
+import slugify
 
 class Article:
-    def __inint__(self, title):
+    def __init__(self, title):
         self.title = title
         self.content = ""
     
     @property 
     def slug(self):
-        return slugify(self.title)
+        return slugify.slugify(self.title)
     
     def load_content(self):
-        with open(f"artice/{self.title}") as file:
+        with open(f"articles/{self.title}") as file:
             self.content = file.read()
     
-    
-    def all():
+    @classmethod
+    def all(cls):
         titles = os.listdir("articles")
         slug_articles = {}
         for title in titles:
-            slug = slugify(title)
-            article = Article(title)
+            slug = slugify.slugify(title)
+            article = cls(title)
             article.load_content()
             slug_articles[slug]=article
         return slug_articles
